2ff91381ac7e4b4acc6003b6eedf6754f4c2f657
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-04 03:08:51 +0100

    added a relationship between the contributor and the book classes and started working on the review table

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 
@@ -14,9 +15,31 @@ class Book(models.Model):
     title = models.CharField(max_length=70, help_text="title of the book")
     publication_date = models.DateField(verbose_name="Date the book was published")
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField('Contributor', through='BookContributor')
 
 class Contributor(models.Model):
     """the class representation of the contributor table"""
     first_names = models.CharField(max_length=50, help_text="The contributor's first name(s)")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name(s)")
     email = models.EmailField(help_text="The contributor's contact email")
+
+class BookContributor(models.Model):
+    """the class representation of the many-to-many relationship
+        between the book table and the contributor table"""
+    class ContributorRoles(models.TextChoices):
+        """a class representation of the enumerated list of choices
+            for the contributor roles"""
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(verbose_name="the role of the contributor",
+                            choices=ContributorRoles.choices, max_length=20)
+
+class Review(models.Model):
+    """the class representation of the review table"""
+    content = models.TextField(help_text="Review text")
+    rating = models.IntegerField(help_text="The rating score")
